f1e18891756f8930a47031b3f9f78a45c21ddd4a
Loïc Jaouen <loic.jaouen@unil.ch>
2020-06-07 01:27:12 +0200

    empty component and routes for calendar and knora access, ref: # 53

---
+++ site_2020/code/site2020/src/app/services/knora.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { KnoraService } from './knora.service';
+
+describe('KnoraService', () => {
+  let service: KnoraService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(KnoraService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
