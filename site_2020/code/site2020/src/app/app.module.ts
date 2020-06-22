ba56a3612bb50dac1fcf01d9ea0f35e5160c5128
Loïc Jaouen <loic.jaouen@unil.ch>
2020-06-22 10:25:32 +0200

    empty year component
    bare result of:
    `ng generate component components/calendarPage`

--- site_2020/code/site2020/src/app/app.module.ts
+++ site_2020/code/site2020/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { CalendarComponent } from "./components/calendar/calendar.component";
+import { CalendarPageComponent } from './components/calendar-page/calendar-page.component';
 
 @NgModule({
-  declarations: [AppComponent, CalendarComponent],
+  declarations: [AppComponent, CalendarComponent, CalendarPageComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ site_2020/code/site2020/src/app/components/calendar-page/calendar-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tds-calendar-page',
+  templateUrl: './calendar-page.component.html',
+  styleUrls: ['./calendar-page.component.scss']
+})
+export class CalendarPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
