b385b9104292f073b44f6044cd8aaab71dc5ef7b
Loïc Jaouen <loic.jaouen@unil.ch>
2020-06-22 10:29:00 +0200

    adding linking to page component

--- site_2020/code/site2020/src/app/app-routing.module.ts
+++ site_2020/code/site2020/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { CalendarComponent } from "./components/calendar/calendar.component";
+import { CalendarPageComponent } from './components/calendar-page/calendar-page.component';
 
-const routes: Routes = [{ path: "calendar", component: CalendarComponent }];
+const routes: Routes = [
+  { path: "calendar", component: CalendarComponent },
+  { path: "year/:year", component: CalendarPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
