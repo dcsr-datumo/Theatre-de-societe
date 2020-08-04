6998ea6534d6f427ba7c1d29761f1a671ece5e5f
Loïc Jaouen <loic.jaouen@unil.ch>
2020-08-04 20:38:57 +0200

    making linter happy

--- site_2020/code/site2020/src/app/app.module.ts
+++ site_2020/code/site2020/src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { CalendarComponent } from "./components/calendar/calendar.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
 import { CalendarPageComponent } from './components/calendar-page/calendar-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
