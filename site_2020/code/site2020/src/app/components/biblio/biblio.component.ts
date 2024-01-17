01caa5a193d1d0727d9fa1ed1057076879e7ff4a
Mahmoud Assaf <assaf_mahmoud@hotmail.com>
2024-01-17 13:36:08 +0100

    Test biblio download link

--- site_2020/code/site2020/src/app/components/biblio/biblio.component.ts
+++ site_2020/code/site2020/src/app/components/biblio/biblio.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { SiteService } from 'src/app/services/site.service';
 
@@ -9,11 +10,21 @@ import { SiteService } from 'src/app/services/site.service';
 export class BiblioComponent implements OnInit {
   site: SiteService;
 
-  constructor(private siteService: SiteService) {
+  fileToDownload = "http://localhost:4200/assets/doc/Biblio_TheatresDeSociete.pdf";
+
+  constructor(private siteService: SiteService, private httpClient: HttpClient) {
     this.site = siteService;
   }
 
   ngOnInit(): void {
   }
+
+  // download(filters: any) {
+  //   console.log("download called...");
+  //   const httpOptions: Object = {
+  //     responseType: 'blob'
+  //   };
+  //   return this.httpClient.post<HttpResponse<Blob>>(filters, filters, httpOptions)
+  // }
 
 }
